635ab6fa62d6ed49eb35cba411652bcc7e707351
pedrotiringa <142243433+pedrotiringa@users.noreply.github.com>
2024-10-22 11:03:02 -0300

    Add files via upload

---
+++ bkpCart-oAlura-main/perguntas.js
@@ -0,0 +1,26 @@
+criaCartao(
+//categoria,
+'Progamação'
+//pergunta,
+'O que é JavaScript'
+//resposta,
+'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    //categoria,
+    'Progamação'
+    //pergunta,
+    'O que é JavaScript'
+    //resposta,
+    'O JavaScript é uma linguagem de programação'
+    )
+
+criaCartao(
+        //categoria,
+        'Progamação'
+        //pergunta,
+        'O que é JavaScript'
+        //resposta,
+        'O JavaScript é uma linguagem de programação'
+        )
